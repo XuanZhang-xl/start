e28668fd11fcd20322df2a6f164a9d7213435f65
xzhang <531126551@qq.com>
2019-08-17 22:35:56 +0800

    重新改为聚合工程

--- test/src/main/java/xl/start/test/config/zk/ZKEvent.java
+++ test/src/main/java/xl/start/test/config/zk/ZKEvent.java
@@ -2,9 +2,18 @@ package xl.start.test.config.zk;
 
 import org.apache.zookeeper.Watcher;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * created by XUAN on 2019/8/13
  */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface ZKEvent {
     Watcher.Event.EventType value();
 }
--- test/src/main/java/xl/start/test/controller/MvcController.java
+++ test/src/main/java/xl/start/test/controller/MvcController.java
@@ -1,11 +1,12 @@
-package xl.starttest.controller;
+package xl.start.test.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import xl.start.test.entity.User;
 import xl.start.test.vo.ResponseVo;
 
@@ -14,7 +15,8 @@ import java.util.concurrent.Callable;
 /**
  * created by XUAN on 2019/05/18
  */
-@Controller("/mvcController")
+@RestController("mvcController")
+@RequestMapping("/mvc")
 public class MvcController {
 
     private static final Logger logger = LoggerFactory.getLogger(MvcController.class);
@@ -23,10 +25,20 @@ public class MvcController {
      * 测试表单提交
      * @return
      */
-    @RequestMapping("/form")
+    @PostMapping("/form")
     public ResponseVo formCommit(User user) {
         logger.info("接受到参数: {}", user);
         return new ResponseVo();
+    }
+
+    /**
+     * 测试时候通畅
+     * @return
+     */
+    @GetMapping("/submit")
+    public ResponseVo submit(String data) {
+        logger.info("接受到data: {}", data);
+        return ResponseVo.buildSuccess(data);
     }
 
     @PostMapping("/asyn")
