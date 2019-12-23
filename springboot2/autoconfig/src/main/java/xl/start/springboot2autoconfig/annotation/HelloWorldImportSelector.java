e85c08de5bccac5b6b81adb676dc574c7157ae31
xuan <xuan.zhang@baozun.com>
2019-12-23 14:05:01 +0800

    明确 AnnotationMetadata

--- springboot2/autoconfig/src/main/java/xl/start/springboot2autoconfig/annotation/HelloWorldImportSelector.java
+++ springboot2/autoconfig/src/main/java/xl/start/springboot2autoconfig/annotation/HelloWorldImportSelector.java
@@ -14,9 +14,9 @@ public class HelloWorldImportSelector implements ImportSelector {
 
 
     /**
-     * 这里传入的  AnnotationMetadata  是 class xl.start.springboot2.bootstrap.EnableHelloWorldBootstrap 的包装类
-     * <p>
-     * 也就是说这方法的意思是返回启动类所需要的所有bean的class的名字?
+     * 这里传入的  AnnotationMetadata  是 xl.start.springboot2autoconfig.configuration.HelloWorldAutoConfiguration 类的元信息包装类
+     *
+     * 因为 @EnableHelloWorld 标注在HelloWorldAutoConfiguration类上, 而@EnableHelloWorld导入了这个ImportSelector
      *
      * @param importingClassMetadata
      * @return
