82e7c2b0ff37eacce5c70310392a095174abb80a
xzhang <531126551@qq.com>
2020-01-03 00:17:24 +0800

    java agent

---
+++ aop/aop-agent/src/main/java/xl/start/aopagent/PerfMonXformer.java
@@ -0,0 +1,54 @@
+package xl.start.aopagent;
+
+import javassist.CannotCompileException;
+import javassist.ClassPool;
+import javassist.CtBehavior;
+import javassist.CtClass;
+
+import java.io.ByteArrayInputStream;
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
+import java.security.ProtectionDomain;
+
+/**
+ * created by XUAN on 2020/1/2
+ */
+public class PerfMonXformer implements ClassFileTransformer {
+
+    @Override
+    public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
+        byte[] transformed = null;
+        System.out.println("transforming " + className);
+        ClassPool pool = ClassPool.getDefault();
+        CtClass cl = null;
+        try {
+            cl = pool.makeClass(new ByteArrayInputStream(classfileBuffer));
+            if (!cl.isInterface()) {
+                CtBehavior[] methods = cl.getDeclaredBehaviors();
+                for (int i = 0; i < methods.length; i++) {
+                    if (!methods[i].isEmpty()) {
+                        // 修改字节码
+                        doMethod(methods[i]);
+                        System.out.println(className + "." + methods[i].getMethodInfo().getName() + " has been enhanced");
+                    }
+                    transformed = cl.toBytecode();
+                }
+            }
+        } catch (Exception e) {
+            System.err.println("could not instrument   " + className + ", exception: " + e.getMessage());
+        } finally {
+            if (cl != null) {
+                cl.detach();
+            }
+        }
+        System.out.println("transforming " + className + " finished");
+        return transformed;
+    }
+
+    private void doMethod(CtBehavior method) throws CannotCompileException {
+        method.insertBefore("long stime = System.nanoTime()");
+        method.insertAfter("System.out.println(\"leave\" + method.getName() + \" and time: \" + System.nanoTime()-stime);");
+        System.out.println();
+
+    }
+}
---
+++ aop/aop-main/src/main/java/xl/aop/aopmain/MainClass.java
@@ -0,0 +1,23 @@
+package xl.aop.aopmain;
+
+/**
+ * java -javaagent:aop-agent-0.0.1-SNAPSHOT.jar=Hello -jar aop-main-0.0.1-SNAPSHOT.jar
+ * 听说这么启动的话print方法会被代理
+ *
+ * D:\03_java\01_jarPagkage\maven\repository\org\javassist\javassist\3.20.0-GA\javassist-3.20.0-GA.jar
+ *
+ * 说我这个路径有非法参数, 待测试
+ *
+ * @author XUAN
+ * @since 2020/01/02
+ */
+public class MainClass {
+
+    public static void main(String[] args) {
+        new MainClass().print();
+    }
+
+    public void print () {
+        System.out.println("xl.aop.aopmain.MainClass#print() 已执行");
+    }
+}
